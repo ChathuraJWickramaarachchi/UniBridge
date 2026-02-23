e8a250e660e83669f241fee3caea5f3fc1127390
Chathura J Wickramaarachchi <chathurajanod2003@gmail.com>
2026-02-23 17:35:50 +0530

    setup the mongodb connection
    setup the mongodb connection

--- backend.js
+++ backend.js
@@ -3,3 +3,16 @@ import mongoose from 'mongoose'; //import mongoose
 
 //initializ the backend software to a variable
 const app = express();
+
+//mongodb connection setup 
+mongoose.connect("mongodb+srv://admin:admin1234@cluster0.zzicnuz.mongodb.net/?appName=Cluster0").then(
+    ()=>{
+    console.log("Successfully connected with database");
+    }
+).catch(
+    ()=>{
+        console.log("Database connection failed");
+    }
+    
+)
+
